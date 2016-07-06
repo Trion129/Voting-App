aff4085a0cd647d15305a9b9431f5f48a25b0691
trion129 <parmsingh129@yahoo.com>
2016-07-06 17:00:53 +0530

    Completed most components

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
 var express = require("express");
 var path = require('path');
 var app = express();
+var assert = require('assert');
 var mongodb = require("mongodb");
 app.set('view engine', 'pug');
 
-var votedata=[
-  [
-    {
-      link: "#",
-      head: "Kejriwal vs Modi",
-      description: "Who would win the polls?",
-      options: [["kejriwal",2],["modi",4],["BB ki shines",6]]
-    },
-    {
-      link: "#",
-      head: "Spooderman or superman",
-      description: "Who is better?"
-    },
-    {
-      link: "#",
-      head: "Spooderman or superman",
-      description: "Who is better?"
-    }
-  ],
-  [
-    {
-      link: "#",
-      head: "Kejriwal vs Modi",
-      description: "Who would win the polls?"
-    },
-    {
-      link: "#",
-      head: "Spooderman or superman",
-      description: "Who is better?"
-    },
-    {
-      link: "#",
-      head: "Spooderman or superman",
-      description: "Who is better?"
-    }
-  ]
-];
-
-var url = 'mongodb://localhost:27017/Votes';
-
+var url = 'mongodb://localhost:27017/test';
+var i,j;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+var find = function(criteria, db, callback) {
+   var votes = [];
+   if(criteria == {}){
+     var cursor = db.collection("Votes").find().limit(15);
+  }
+  else{
+     var cursor = db.collection("Votes").find(criteria);
+  }
+
+   cursor.each(function(err, doc) {
+      if (doc != null) {
+         votes.push(doc);
+      } else {
+         callback(votes);
+      }
+   });
+};
+var insert = function(criteria , db, callback) {
+   db.collection('Votes').insertOne(criteria, function(err, result) {
+    assert.equal(err, null);
+    callback(result);
+  });
+};
 
 app.get('/', function (req, res) {
   mongodb.MongoClient.connect(url, function(err,db){
-    
-    db.close();
-  })
-  res.render('webpages/index', { title: 'Votesapp', votedata: JSON.stringify(votedata)});
+    find({}, db, function(votes) {
+      var votedata = [];
+      for(i = 0;i < votes.length;i++){
+        if(votedata[Math.floor(i / 3)]){
+            votedata[Math.floor(i / 3)].push(votes[i]);
+        }
+        else {
+          votedata.push([]);
+          votedata[Math.floor(i / 3)].push(votes[i]);
+        }
+      }
+      res.render('webpages/index', { title: 'Votesapp', votedata: JSON.stringify(votedata)});
+      db.close();
+    });
+  });
 });
 
 app.get('/mypolls', function(req, res){
-  res.render('webpages/mypolls', { title: "My Polls", votedata: JSON.stringify(votedata)});
+  var query = req.query.user;
+  if(!query){
+    res.end("Unspecified User");
+    return;
+  }
+  mongodb.MongoClient.connect(url, function(err,db){
+    find({user: query}, db, function(votes) {
+      var votedata = [];
+      for(i = 0;i < votes.length;i++){
+        if(votedata[Math.floor(i / 3)]){
+            votedata[Math.floor(i / 3)].push(votes[i]);
+        }
+        else {
+          votedata.push([]);
+          votedata[Math.floor(i / 3)].push(votes[i]);
+        }
+      }
+      res.render('webpages/mypolls', { title: "My Polls", votedata: JSON.stringify(votedata)});
+      db.close();
+    });
+  });
 });
 
 app.get('/newpoll', function(req,res){
@@ -63,9 +81,20 @@ app.get('/newpoll', function(req,res){
 });
 
 app.post('/api/addpoll', function(req,res){
-  //TODO Add data
-  id = 0;
-  res.redirect('/viewpoll/' + id);
+  var criteria={
+    title : req.body.title,
+    description : req.body.description,
+    user : req.body.user,
+    options : req.body.options
+  };
+  res.end(criteria);
+  /*MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    insert(criteria,db, function(result) {
+        res.redirect('/viewpoll/' + result._id);
+        db.close();
+    });
+  });*/
 })
 
 app.get('/viewpoll/:param', function(req,res){
